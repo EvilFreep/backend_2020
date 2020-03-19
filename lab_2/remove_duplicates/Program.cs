2ba293895769957f1e2b1ca9d4b98e843f2d96c7
EvilFreep <56558082+EvilFreep@users.noreply.github.com>
2020-03-19 17:58:18 +0300

    added lw_2

---
+++ lab_2/remove_duplicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of argumens!");
+                Console.WriteLine("Right form: remove_duplicates <input string>");
+                return 1;
+            }
+
+            HashSet<char> setOfCharacters = new HashSet<char>();
+            string inputLine = args[0];
+            char ch;
+            for(int i = 0; i < inputLine.Length; i++)
+            {
+                ch = inputLine[i];
+                if (!setOfCharacters.Contains(ch))
+                {
+                    setOfCharacters.Add(ch);
+                }
+                else
+                {
+                    inputLine = inputLine.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(inputLine);
+
+            return 0;
+        }
+    }
+}
