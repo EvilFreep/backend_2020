1da28f26b183e8631963488ed6bd89cd1d1ffff7
EvilFreep <56558082+EvilFreep@users.noreply.github.com>
2020-03-19 16:17:46 +0300

    added lw_2

---
+++ lab_2/printArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace printArgs
+{
+    class MainClass
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+                foreach (string s in args)
+                    Console.Write(s + " ");
+            }
+
+            return 0;
+        }
+    }
+}
